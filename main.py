bef382992ee5fcc5536d3ce08e0438c33fb2d535
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-07 23:12:05 +0300

    handle uknown sounds and connection errors

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ recognizer = sr.Recognizer()
 with sr.Microphone() as source:
     print("Enter English text to translate")
     audio = recognizer.listen(source)
-    voice_data = recognizer.recognize_google(audio)
-    print(voice_data)
+    try:
+        voice_data = recognizer.recognize_google(audio)
+        print(voice_data)
+    except sr.UnknownValueError:
+        print("Sorry, I did not get that")
+    except sr.RequestError:
+        print("Sorry, my services are currently down")
