c2fb9cb630d97b1a14cb613b9183a86ae837e9e6
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-08 00:21:01 +0300

    speak function to output sound from gTTS

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
-import speech_recognition as sr
+import os
+import random
 from pig_latin import pig_latin
+import speech_recognition as sr
+import playsound
+from gtts import gTTS
 
 recognizer = sr.Recognizer()
 
@@ -11,18 +15,25 @@ def record_audio():
         try:
             voice_data = recognizer.recognize_google(audio)
         except sr.UnknownValueError:
-            print("Sorry, I did not get that")
+            speak("Sorry, I did not get that")
         except sr.RequestError:
-            print("Sorry, my services are currently down")
+            speak("Sorry, my services are currently down")
         return voice_data
 
 def translate(voice_data):
     translated_text = pig_latin(voice_data)
-    print(translated_text)
+    speak(translated_text)
+
+def speak(audio_string):
+    text_to_speech = gTTS(text=audio_string, lang='en')
+    num = random.randint(1,10000000)
+    audio_file = 'audio-' + str(num) + '.mp3'
+    text_to_speech.save(audio_file)
+    playsound.playsound(audio_file)
+    os.remove(audio_file)
 
 time.sleep(1)
-print("Enter English text to translate to pig latin")
+speak("Enter English text to translate to pig latin")
 while True:
     voice_data = record_audio()
-    print(voice_data)
     translate(voice_data)
