ec433e36cfe09385b2871e55be231eb71518b404
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-07 23:16:24 +0300

    record audio function - call it to get voice data

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Enter English text to translate")
-    audio = recognizer.listen(source)
-    try:
-        voice_data = recognizer.recognize_google(audio)
-        print(voice_data)
-    except sr.UnknownValueError:
-        print("Sorry, I did not get that")
-    except sr.RequestError:
-        print("Sorry, my services are currently down")
+def record_audio():
+    with sr.Microphone() as source:
+        audio = recognizer.listen(source)
+        voice_data = ''
+        try:
+            voice_data = recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Sorry, I did not get that")
+        except sr.RequestError:
+            print("Sorry, my services are currently down")
+        return voice_data
+
+print("Enter English text to translate to pig latin")
+voice_data = record_audio()
+print(voice_data)
